ae1de0a7349adbf7f2a02778a16d6cc74ac3885a
bishalpdl <bishal7paudel@gmail.com>
2023-07-31 11:26:47 +0545

    category blog rel

--- backend/route.js
+++ backend/route.js
@@ -3,6 +3,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import productRouter from "./routes/product.route.js";
 import blogRouter from "./routes/blog.route.js";
+import categoryRouter from "./routes/category.route.js";
 
 const mainRouter = Router({ mergeParams: true });
 
@@ -10,6 +11,8 @@ mainRouter.use("/user", userRouter); //localhost:8000/api/user
 mainRouter.use("/auth", authRouter);
 
 mainRouter.use("/product", productRouter);
+
+mainRouter.use("/category", categoryRouter);
 
 mainRouter.use("/blog", blogRouter);
 
